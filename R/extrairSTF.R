2a7bd36af2d587b487b1e091f72cea2d93c1867c
Jonathan Morais Barcellos Ferreira <jonathanmbferreira@outlook.com>
2023-06-16 22:15:57 -0300

    Cria a função extrairSTF.info

--- R/extrairSTF.R
+++ R/extrairSTF.R
@@ -82,11 +82,109 @@ extrairSTF.partes = function(lista, classe, n, UA){
     names(data$Classe) <- classe
     lista[[i]] <- data
 
+    # Aguarde 15 segundos
     date_time<-Sys.time()
     while((as.numeric(Sys.time()) - as.numeric(date_time))<15){}
   }
 
   # Devolva a lista com os processos
   return(lista)
+
+}
+
+
+#' Extrair data do protocolo e assunto do processo
+#'
+#' @description
+#' `extrairSTF.info()` permite extrair do site do Supremo Tribunal Federal dados de ajuizamento e assunto dos processos.
+#'
+#' @param lista Uma lista
+#' @param classe Um caracter que indica a classe processual
+#' @param n Um número que indica o número do processo
+#' @param UA User-Agent
+#'
+#' @return Uma lista com as datas de ajuizamento e assuntos requeridos.
+#' @export
+#'
+#' @examples
+#' # Extrair os dados da ADPF 800
+#' \dontrun{
+#' extrairSTF.info(lista, "ADPF", 800, UA)
+#'}
+#'
+#' # Extrair os dados da ADI 500 a 600
+#' \dontrun{
+#' extrairSTF.info(lista, "ADI", 500:600, UA)
+#' }
+extrairSTF.info = function(lista, classe, n, UA){
+
+  for(i in n){ ## faça isso em cada processo ##
+
+    # Busque no site o processo
+    getProcesso <- httr::GET(
+      paste(
+        'https://portal.stf.jus.br/processos/listarProcessos.asp?classe=',
+        classe,
+        '&numeroProcesso=',
+        i,
+        sep = ''
+      ),
+      httr::add_headers(
+        'User-Agent' = UA
+      )
+    )
+
+    # Salve o incidente
+    getIncidente <- stringr::str_split_i(
+      getProcesso$url,
+      pattern = '=',
+      -1
+    )
+
+    # Buscar na aba informações
+    getInfo <- httr::GET(
+      paste('https://portal.stf.jus.br/processos/abaInformacoes.asp?incidente=',
+            getIncidente,
+            sep = ''),
+      httr::add_headers(
+        'User-Agent' = UA
+      )
+    )
+
+    # Leia o conteúdo da aba informações
+    c <- httr::content(b, encoding = 'UTF-8')
+
+    # Salve o conteúdo da aba informações
+    ## Data do protocolo (ajuizamento)
+    getProtocolo <- xml2::xml_find_all(
+      c,
+      "//div[@class='col-md-5 processo-detalhes-bold m-l-0']"
+    ) %>% xml2::xml_text(trim = T)
+
+    ## Assunto do processo
+    getAssunto <- xml2::xml_find_all(
+      c,
+      "//div[@class='col-md-10 processo-detalhes']"
+    ) %>% xml2::xml_text(trim = T)
+
+    # Coloque os dados em uma tabela
+    data <- data.frame(
+      Classe = i,
+      Ajuizamento = getProtocolo,
+      Assunto = getAssunto
+    )
+
+    # Coloque os dados em uma lista
+    data$i <- i
+    names(data$Classe) <- classe
+    lista[[i]] <- data
+
+    # Aguarde 15 segundos
+    date_time<-Sys.time()
+    while((as.numeric(Sys.time()) - as.numeric(date_time))<15){}
+  }
+
+  # Devolva a lista com os processos
+  return(datalist)
 
 }
