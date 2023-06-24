3b87d3be787a65a9458937d3cc5758d2c5b1a4a5
Jonathan Morais Barcellos Ferreira <jonathanmbferreira@outlook.com>
2023-06-24 01:32:55 -0300

    cf. commit anterior

---
+++ R/data.R
@@ -0,0 +1,64 @@
+#' Dicionários de expressões com 6 palavras
+#'
+#' Conjunto de expressões frequentes em decisões judiciais para serem aplicadas sobre tokens para facilitar na análise textual de decisões judiciais.
+#'
+#' @format ## `dic_6grams`
+#' Um data frame com 1 coluna e 9 linhas:
+#' \describe{
+#'  \item{V1}{Expressões}
+#' }
+#'
+#' @source Elaborado por Jonathan Ferreira
+"dic_6grams"
+
+#' Dicionários de expressões com 5 palavras
+#'
+#' Conjunto de expressões frequentes em decisões judiciais para serem aplicadas sobre tokens para facilitar na análise textual de decisões judiciais.
+#'
+#' @format ## `dic_5grams`
+#' Um data frame com 1 coluna e 1 linha:
+#' \describe{
+#'  \item{V1}{Expressões}
+#' }
+#'
+#' @source Elaborado por Jonathan Ferreira
+"dic_5grams"
+
+#' Dicionários de expressões com 4 palavras
+#'
+#' Conjunto de expressões frequentes em decisões judiciais para serem aplicadas sobre tokens para facilitar na análise textual de decisões judiciais.
+#'
+#' @format ## `dic_4grams`
+#' Um data frame com 1 coluna e 14 linhas:
+#' \describe{
+#'  \item{V1}{Expressões}
+#' }
+#'
+#' @source Elaborado por Jonathan Ferreira
+"dic_4grams"
+
+#' Dicionários de expressões com 3 palavras
+#'
+#' Conjunto de expressões frequentes em decisões judiciais para serem aplicadas sobre tokens para facilitar na análise textual de decisões judiciais.
+#'
+#' @format ## `dic_3grams`
+#' Um data frame com 1 coluna e 15 linhas:
+#' \describe{
+#'  \item{V1}{Expressões}
+#' }
+#'
+#' @source Elaborado por Jonathan Ferreira
+"dic_3grams"
+
+#' Dicionários de expressões com 2 palavras
+#'
+#' Conjunto de expressões frequentes em decisões judiciais para serem aplicadas sobre tokens para facilitar na análise textual de decisões judiciais.
+#'
+#' @format ## `dic_2grams`
+#' Um data frame com 1 coluna e 41 linhas:
+#' \describe{
+#'  \item{V1}{Expressões}
+#' }
+#'
+#' @source Elaborado por Jonathan Ferreira
+"dic_2grams"
