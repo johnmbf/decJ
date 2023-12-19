e9edf579cfa996d55e390c963307d30890bb7721
Jonathan Morais Barcellos Ferreira <jonathanmbferreira@outlook.com>
2023-12-19 00:21:26 -0300

    corrige erro com o argumento classe da função stf_partes

--- R/stf.R
+++ R/stf.R
@@ -11,6 +11,7 @@
 #'
 #' @examples
 stf_partes = function(classe, processo){
+  classe <- base::toupper(classe)
   header <- httr::add_headers("User-Agent" = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.51")
 
   purrr::map_dfr(processo, purrr::slowly(~{
